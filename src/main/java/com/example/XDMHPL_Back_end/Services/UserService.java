2a2c721cfec2728e1880eabdf610bd8a77c10ab3
nhoangvietcuong <sitinhboi782@gmail.com>
2025-05-04 16:23:37 +0700

    Update UserService.java

--- src/main/java/com/example/XDMHPL_Back_end/Services/UserService.java
+++ src/main/java/com/example/XDMHPL_Back_end/Services/UserService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.example.XDMHPL_Back_end.DTO.UserDTO;
+
 import com.example.XDMHPL_Back_end.Repositories.UserRepository;
 import com.example.XDMHPL_Back_end.model.Users;
 
