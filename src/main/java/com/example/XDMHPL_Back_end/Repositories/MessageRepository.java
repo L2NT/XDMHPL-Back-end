cb4131d752a7e3447b168d5c05730ff9e07d06ea
nhoangvietcuong <sitinhboi782@gmail.com>
2025-05-04 16:20:32 +0700

    Update MessageRepository.java

--- src/main/java/com/example/XDMHPL_Back_end/Repositories/MessageRepository.java
+++ src/main/java/com/example/XDMHPL_Back_end/Repositories/MessageRepository.java
@@ -1,12 +1,14 @@
 package com.example.XDMHPL_Back_end.Repositories;
 
-import com.example.XDMHPL_Back_end.DTO.Message;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.XDMHPL_Back_end.model.MessageModel;
 
 import java.util.List;
 
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-    public List<Message> findByChatBox_ChatBoxID(Integer chatBoxId);
+public interface MessageRepository extends JpaRepository<MessageModel, Integer> {
+    public List<MessageModel> findByChatBox_ChatBoxID(Integer chatBoxId);
 
 
 }
