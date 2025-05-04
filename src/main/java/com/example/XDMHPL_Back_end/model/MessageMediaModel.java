0f79c88ff7755097f27c57035468a5448420ecc7
nhoangvietcuong <sitinhboi782@gmail.com>
2025-05-04 16:20:54 +0700

    Update MessageMediaModel.java

--- src/main/java/com/example/XDMHPL_Back_end/model/MessageMediaModel.java
+++ src/main/java/com/example/XDMHPL_Back_end/model/MessageMediaModel.java
@@ -1,7 +1,5 @@
 package com.example.XDMHPL_Back_end.model;
 
-import com.example.XDMHPL_Back_end.DTO.Message;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,52 +10,59 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "messagemedia")
-public class MessageMediaModel {
+ @Table(name = "messagemedia")
+ public class MessageMediaModel {
+ 
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private int messageMediaID;
+ 
+     @Column(name = "MediaType")
+     private String mediaType;
+ 
+     @Column(name = "MediaURL")
+     private String mediaURL;
+ 
+     @ManyToOne
+     @JoinColumn(name = "MessageID")
+     private MessageModel message;
+ 
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int messageMediaID;
-
-    @Column(name = "MediaType")
-    private String mediaType;
-
-    @Column(name = "MediaURL")
-    private String mediaURL;
-
-    @ManyToOne
-    @JoinColumn(name = "MessageID")
-    private Message message;
-
-    public int getMessageMediaID() {
-        return messageMediaID;
-    }
-
-    public void setMessageMediaID(int messageMediaID) {
+     public MessageMediaModel(int messageMediaID, String mediaType, String mediaURL, MessageModel message) {
         this.messageMediaID = messageMediaID;
-    }
-
-    public String getMediaType() {
-        return mediaType;
-    }
-
-    public void setMediaType(String mediaType) {
         this.mediaType = mediaType;
-    }
-
-    public String getMediaURL() {
-        return mediaURL;
-    }
-
-    public void setMediaURL(String mediaURL) {
         this.mediaURL = mediaURL;
-    }
-
-    public Message getMessage() {
-        return message;
-    }
-
-    public void setMessage(Message message) {
         this.message = message;
     }
-}
+     public int getMessageMediaID() {
+         return messageMediaID;
+     }
+ 
+     public void setMessageMediaID(int messageMediaID) {
+         this.messageMediaID = messageMediaID;
+     }
+ 
+     public String getMediaType() {
+         return mediaType;
+     }
+ 
+     public void setMediaType(String mediaType) {
+         this.mediaType = mediaType;
+     }
+ 
+     public String getMediaURL() {
+         return mediaURL;
+     }
+ 
+     public void setMediaURL(String mediaURL) {
+         this.mediaURL = mediaURL;
+     }
+ 
+     public MessageModel getMessage() {
+         return message;
+     }
+ 
+     public void setMessage(MessageModel message) {
+         this.message = message;
+     }
+ }
