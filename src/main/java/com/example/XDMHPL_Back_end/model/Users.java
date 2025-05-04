faea487fd9f35758d37eccaecafea801a3e16391
nhoangvietcuong <sitinhboi782@gmail.com>
2025-05-04 16:23:43 +0700

    Update Users.java

--- src/main/java/com/example/XDMHPL_Back_end/model/Users.java
+++ src/main/java/com/example/XDMHPL_Back_end/model/Users.java
@@ -11,85 +11,331 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
-@AllArgsConstructor
+
 @Data
 public class Users {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int userID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
 
-	@Column(name = "`FullName`", nullable = true, unique = false)
-	private String fullName;
+    @Column(name = "`FullName`", nullable = true, unique = false)
+    private String fullName;
 
-	@Column(name = "UserName", nullable = true, unique = true)
-	private String userName;
+    @Column(name = "UserName", nullable = true, unique = true)
+    private String userName;
 
-	@Column(name = "Email", nullable = true, unique = true)
-	private String email;
+    @Column(name = "Email", nullable = true, unique = true)
+    private String email;
 
-	@Column(name = "Password", nullable = true)
-	private String password;
+    @Column(name = "Password", nullable = true)
+    private String password;
 
-	@Column(name = "AvatarURL", nullable = true)
-	private String avatar;
-	
-	@Column(name = "PhoneNumber", nullable = true)
-	private String phoneNumber;
-	
-	@Column(name = "DateOfBirth", nullable = true)
-	private LocalDate dateOfBirth;
-	
-	@Column(name = "Gender", nullable = true)
-	private String gender;
-	
-	@Column(name = "CoverPhotoURL", nullable = true)
-	private String coverPhotoURL;
-	
-	@Column(name = "SessionID", nullable = true)
-	private String sessionID;
+    @Column(name = "AvatarURL", nullable = true)
+    private String avatar;
 
-	@Column(name ="Bio", nullable = true)
-	private String Bio;
-	
-	@Column(name = "Role", nullable = true)
-	private String role;
-	
-	@Column(name = "Hide", nullable = true)
-	private boolean hide;
+    @Column(name = "PhoneNumber", nullable = true)
+    private String phoneNumber;
 
-	@Column(name = "is_online", nullable = false)
+    @Column(name = "DateOfBirth", nullable = true)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "Gender", nullable = true)
+    private String gender;
+
+    @Column(name = "CoverPhotoURL", nullable = true)
+    private String coverPhotoURL;
+
+    @Column(name = "SessionID", nullable = true)
+    private String sessionID;
+
+    @Column(name ="Bio", nullable = true)
+    private String Bio;
+
+    @Column(name = "Role", nullable = true)
+    private String role;
+
+    @Column(name = "Hide", nullable = true)
+    private boolean hide;
+
+    @Column(name = "is_online", nullable = false)
     private Boolean isOnline = false;
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Notification> notifications; // Danh sách thông báo nhận được
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Notification> notifications; // Danh sách thông báo nhận được
 
-	@OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Notification> sentNotifications; // Danh sách thông báo đã gửi
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Friend> friends; // Danh sách bạn bè
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Friend> friends; // Danh sách bạn bè
 
-	@OneToMany(mappedBy = "friendUser", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Friend> friendOf; // Danh sách người mà user là bạn bè
+    @OneToMany(mappedBy = "friendUser", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Friend> friendOf; // Danh sách người mà user là bạn bè
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Follower> followers; // Danh sách người theo dõi
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Follower> followers; // Danh sách người theo dõi
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Following> followings; // Danh sách người đang theo dõi
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Following> followings; // Danh sách người đang theo dõi
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Session> sessions; // Danh sách đăng nhập trên thiết bị
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Session> sessions; // Danh sách đăng nhập trên thiết bị
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<ChatBoxDetail> chatBoxDetails; // Danh sách các ChatBoxDetail liên kết với User
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ChatBoxDetail> chatBoxDetails; // Danh sách các ChatBoxDetail liên kết với User
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Comment> Comments; // Danh sách các bài đã likes của User
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> Comments; // Danh sách các bài đã likes của User
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Like> Likes; // Danh sách các bài đã likes của User
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Like> Likes; // Danh sách các bài đã likes của User
 
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Post> Posts; // Danh sách các bài đã likes của User
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> Posts; // Danh sách các bài đã likes của User
+
+    // Constructor with parameters
+    public Users(int userID, String fullName, String userName, String email, String password, String avatar,
+                 String phoneNumber, LocalDate dateOfBirth, String gender, String coverPhotoURL, String sessionID,
+                 String Bio, String role, boolean hide, Boolean isOnline,
+                 List<Notification> notifications, List<Notification> sentNotifications,
+                 List<Friend> friends, List<Friend> friendOf, List<Follower> followers,
+                 List<Following> followings, List<Session> sessions, List<ChatBoxDetail> chatBoxDetails,
+                 List<Comment> comments, List<Like> likes, List<Post> posts) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.avatar = avatar;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.coverPhotoURL = coverPhotoURL;
+        this.sessionID = sessionID;
+        this.Bio = Bio;
+        this.role = role;
+        this.hide = hide;
+        this.isOnline = isOnline;
+        this.notifications = notifications;
+        this.sentNotifications = sentNotifications;
+        this.friends = friends;
+        this.friendOf = friendOf;
+        this.followers = followers;
+        this.followings = followings;
+        this.sessions = sessions;
+        this.chatBoxDetails = chatBoxDetails;
+        this.Comments = comments;
+        this.Likes = likes;
+        this.Posts = posts;
+    }
+
+    // Getter and Setter methods
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCoverPhotoURL() {
+        return coverPhotoURL;
+    }
+
+    public void setCoverPhotoURL(String coverPhotoURL) {
+        this.coverPhotoURL = coverPhotoURL;
+    }
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public String getBio() {
+        return Bio;
+    }
+
+    public void setBio(String bio) {
+        Bio = bio;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isHide() {
+        return hide;
+    }
+
+    public void setHide(boolean hide) {
+        this.hide = hide;
+    }
+
+    public Boolean getIsOnline() {
+        return isOnline;
+    }
+
+    public void setIsOnline(Boolean isOnline) {
+        this.isOnline = isOnline;
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    public List<Notification> getSentNotifications() {
+        return sentNotifications;
+    }
+
+    public void setSentNotifications(List<Notification> sentNotifications) {
+        this.sentNotifications = sentNotifications;
+    }
+
+    public List<Friend> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<Friend> friends) {
+        this.friends = friends;
+    }
+
+    public List<Friend> getFriendOf() {
+        return friendOf;
+    }
+
+    public void setFriendOf(List<Friend> friendOf) {
+        this.friendOf = friendOf;
+    }
+
+    public List<Follower> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<Follower> followers) {
+        this.followers = followers;
+    }
+
+    public List<Following> getFollowings() {
+        return followings;
+    }
+
+    public void setFollowings(List<Following> followings) {
+        this.followings = followings;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public List<ChatBoxDetail> getChatBoxDetails() {
+        return chatBoxDetails;
+    }
+
+    public void setChatBoxDetails(List<ChatBoxDetail> chatBoxDetails) {
+        this.chatBoxDetails = chatBoxDetails;
+    }
+
+    public List<Comment> getComments() {
+        return Comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        Comments = comments;
+    }
+
+    public List<Like> getLikes() {
+        return Likes;
+    }
+
+    public void setLikes(List<Like> likes) {
+        Likes = likes;
+    }
+
+    public List<Post> getPosts() {
+        return Posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        Posts = posts;
+    }
 }
