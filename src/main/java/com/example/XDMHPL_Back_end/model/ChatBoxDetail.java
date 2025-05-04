a5ebe0c809fe64fbd8ac07df888839c5d1dd7488
nhoangvietcuong <sitinhboi782@gmail.com>
2025-05-04 16:21:10 +0700

    Update ChatBoxDetail.java

--- src/main/java/com/example/XDMHPL_Back_end/model/ChatBoxDetail.java
+++ src/main/java/com/example/XDMHPL_Back_end/model/ChatBoxDetail.java
@@ -1,35 +1,56 @@
 package com.example.XDMHPL_Back_end.model;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
+import jakarta.persistence.*;
 
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "chatboxdetail")
 public class ChatBoxDetail {
-    @EmbeddedId
-    private ChatBoxDetailId id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chatboxdetailId")
+    private Integer chatBoxDetailId;
 
     @ManyToOne
-    @MapsId("userID")
-    @JoinColumn(name = "UserID")
+    @JoinColumn(name = "UserID", nullable = false)
     private Users user;
 
     @ManyToOne
-    @MapsId("chatBoxID")
-    @JoinColumn(name = "ChatBoxID")
+    @JoinColumn(name = "ChatBoxID", nullable = false)
     private ChatBox chatBox;
 
+    // Constructors
+    public ChatBoxDetail() {
+    }
+
+    public ChatBoxDetail(Integer chatBoxDetailId, Users user, ChatBox chatBox) {
+        this.chatBoxDetailId = chatBoxDetailId;
+        this.user = user;
+        this.chatBox = chatBox;
+    }
+
+    // Getters and Setters
+    public Integer getChatBoxDetailId() {
+        return chatBoxDetailId;
+    }
+
+    public void setChatBoxDetailId(Integer chatBoxDetailId) {
+        this.chatBoxDetailId = chatBoxDetailId;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public ChatBox getChatBox() {
+        return chatBox;
+    }
+
+    public void setChatBox(ChatBox chatBox) {
+        this.chatBox = chatBox;
+    }
 }
-
-
