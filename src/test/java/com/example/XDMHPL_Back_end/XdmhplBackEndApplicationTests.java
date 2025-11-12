05d427c47cdbd4d5ecafc43bcc8a39c901ab487f
L2NT <lnntam04@gmail.com>
2025-11-12 18:34:06 +0700

    Initial commit

--- src/test/java/com/example/XDMHPL_Back_end/XdmhplBackEndApplicationTests.java
+++ src/test/java/com/example/XDMHPL_Back_end/XdmhplBackEndApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.XDMHPL_Back_end;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Integration test - requires database")
 class XdmhplBackEndApplicationTests {
 
 	@Test
